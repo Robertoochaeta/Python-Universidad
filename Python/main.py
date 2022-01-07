5208b4410e8496b28478efbe624073d6bb601b8f
Roberto <mario24roberto@gmail.com>
2022-01-06 21:17:03 -0600

    Utilizando valores booleanos

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,6 @@
-# cadena
-miGrupoFavorito = "Metallica"
-comentario = "The Best Rock Band"
-print("Mi Grupo Favorito es: ", miGrupoFavorito, comentario)
+#Tipos Booleano o bool
+miVariable = True
+print(miVariable)
 
-numero1 = "1"
-numero2 = "2"
-print(int(numero1) + int(numero2))
-# numero1=3
-# numero2 = 4
-# print(numero1 + numero2)
+miVariable =3 > 2
+print(miVariable)
