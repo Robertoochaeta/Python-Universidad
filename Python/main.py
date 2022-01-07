2ee7eb6aed79fdd8152c8f4652e2c596f433d40d
Roberto <mario24roberto@gmail.com>
2022-01-06 22:03:06 -0600

    Utilizando la funcion input para concatenar

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,5 @@
-#Funcion Input para procesar los valores
-resultado = input("Escribe un Mensaje:")
-print("Valor proporcionado " ,resultado)
+# Funcion Input para procesar los valores
+numero1 = input("Escribe el primer Numero :")
+numero2 = input("Escribe el segundo Numero: ")
+resultado = numero1 + numero2
+print("El valor de la suma es:  ", resultado)
